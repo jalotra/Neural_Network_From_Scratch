05ceea729c678a82d1d2f6ccc0cad528daedff97
Shivam <jalotrashivam9@gmail.com>
2019-10-04 21:39:54 +0530

    Added Neural network methods

--- uwimg.py
+++ uwimg.py
@@ -1,7 +1,7 @@
 from ctypes import *
 import math
 import random
-import os
+import os,sys
 # import sys
 
 test_photos_directory = "test_photos/"
@@ -61,6 +61,82 @@ def create_test_photos_directory():
         os.mkdir(path = os.path.join(os.getcwd(), test_photos_directory), mode = 777 )
     except FileExistsError:
         pass 
+
+
+
+'''
+NEURAL NETWORK STUFF '''
+class IMAGE(Structure):
+    _fields_ = [("w", c_int),
+                ("h", c_int),
+                ("c", c_int),
+                ("data", POINTER(c_float))]
+    def __add__(self, other):
+        return add_image(self, other)
+    def __sub__(self, other):
+        return sub_image(self, other)
+
+class POINT(Structure):
+    _fields_ = [("x", c_float),
+                ("y", c_float)]
+
+class DESCRIPTOR(Structure):
+    _fields_ = [("p", POINT),
+                ("n", c_int),
+                ("data", POINTER(c_float))]
+
+class MATRIX(Structure):
+    _fields_ = [("rows", c_int),
+                ("cols", c_int),
+                ("data", POINTER(POINTER(c_double))),
+                ("shallow", c_int)]
+
+class DATA(Structure):
+    _fields_ = [("X", MATRIX),
+                ("y", MATRIX)]
+
+class LAYER(Structure):
+    _fields_ = [("in", MATRIX),
+                ("dw", MATRIX),
+                ("w", MATRIX),
+                ("v", MATRIX),
+                ("out", MATRIX),
+                ("activation", c_int)]
+
+class MODEL(Structure):
+    _fields_ = [("layers", POINTER(LAYER)),
+                ("n", c_int)]
+
+
+(LINEAR, LOGISTIC, RELU, LRELU, SOFTMAX) = range(5)
+
+def make_model(layers):
+    m = MODEL()
+    m.n = len(layers)
+    m.layers = (LAYER*m.n) (*layers)
+    return m
+
+
+train_model = lib.train_model
+train_model.argtypes = [MODEL, DATA, c_int, c_int, c_double, c_double, c_double]
+train_model.restype = None
+
+accuracy_model = lib.accuracy_model
+accuracy_model.argtypes = [MODEL, DATA]
+accuracy_model.restype = c_double
+
+forward_model = lib.forward_model
+forward_model.argtypes = [MODEL, MATRIX]
+forward_model.restype = MATRIX
+
+load_classification_data = lib.load_classification_data
+load_classification_data.argtypes = [c_char_p, c_char_p, c_int]
+load_classification_data.restype = DATA
+
+make_layer = lib.make_layer
+make_layer.argtypes = [c_int, c_int, c_int]
+make_layer.restype = LAYER
+
 
 
 if __name__ == "__main__":
