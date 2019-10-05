28b26cd3f1502e8372fff851097ef9ead89231dd
Shivam <jalotrashivam9@gmail.com>
2019-10-05 07:00:58 +0530

    Results of different combinations

--- neuralNetworkTrainer.py
+++ neuralNetworkTrainer.py
@@ -10,12 +10,13 @@ def neural_net(inputs, outputs):
     print (inputs)
     l = [make_layer(inputs, 32, RELU),
     		make_layer(32, 16,RELU),
+		#make_layer(16, 8, RELU),
             make_layer(16, outputs, SOFTMAX)]
     return make_model(l)
 
 def save_to_file(filepath, process_to_run):
 
-	orig_stderr = sys.stdout
+	orig_stdout = sys.stdout
 	with open('filepath', 'w') as f:
 		sys.stdout = f
 
